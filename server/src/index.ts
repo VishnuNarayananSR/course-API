da048f591b7c479a61f92278efaf6967d9ea2496
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2022-10-09 01:01:15 +0530

    added simple logger middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import bootcamp from "../routes/api/v1/bootcamps";
+import logger from "../middleware/logger";
+
 dotenv.config({ path: "./config/.env" });
 
 const PORT = process.env.PORT || 4000;
 const V1 = "/api/v1";
 const app = express();
+
+app.use(logger);
 
 app.use(V1, bootcamp);
 
