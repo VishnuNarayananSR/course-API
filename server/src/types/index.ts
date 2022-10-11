d1af6cd136702d5c6b27229283fc1f2158eedee5
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2022-10-11 23:01:12 +0530

    JSONResponse generic type added

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,1 +1,5 @@
 export type { default as Bootcamp } from "./bootcamp";
+
+type Success<T> = { success: true; data: T };
+type Failure<E> = { success: false; err: E };
+export type JSONResponse<T> = Success<T> | Failure<T>;
