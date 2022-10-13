5d58d87c48de1f21d208145284f241cd0da4ffd1
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2022-10-13 10:36:44 +0530

    custom message for CastError

--- server/src/middleware/error-handler.ts
+++ server/src/middleware/error-handler.ts
@@ -3,10 +3,13 @@ import { ApiError } from "../types/errors";
 
 export default class ErrorHandler {
   public static handle =
-    () => (err: ApiError, _: Request, res: Response, next: NextFunction) => {
+    () => (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+      let statusCode = err.statusCode;
+      let message = err.message;
+      if (err.name === "CastError") {
+        message = `Incorrect identifier. No Resouce found`;
+      }
       console.log(err.stack);
-      res
-        .status(err.statusCode || 500)
-        .json({ success: false, err: err.message });
+      res.status(statusCode || 500).json({ success: false, err: message });
     };
 }
