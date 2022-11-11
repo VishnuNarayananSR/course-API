6461e46465ac98677e7b23b810a358722bb1763a
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2022-11-11 12:42:06 +0530

    improvized typings in async handler

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,9 +1,14 @@
-export type { default as Bootcamp } from "./bootcamp";
+import { NextFunction, Request, Response } from "express";
 
+export type { default as Bootcamp } from "./bootcamp";
 export interface Countable {
   count: number;
 }
-
+export type Middleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<unknown>;
 type Success<T> = { success: true; data: T };
 type Failure<E> = { success: false; err: E };
 export type JSONResponse<T> = Success<T> | Failure<T>;
