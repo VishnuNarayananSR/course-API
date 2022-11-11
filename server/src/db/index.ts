025ce3c9b5c544f6b49df41ec27f45eea58c26da
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2022-11-11 12:47:08 +0530

    nerfed mongoose to v5

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export default async () => {
   const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/course-db";
-  const conn = await mongoose.connect(uri);
+  const conn = await mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
   console.log("Connected to MongoDB");
   return conn;
 };
